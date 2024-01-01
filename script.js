a13a28d76025c4401e24e94846394ea061e2c40b
tahereh <tahereh@gamil.com>
2024-01-01 04:26:47 -0800

    Update script.js
    More options added to chatbot respond

--- script.js
+++ script.js
@@ -5,14 +5,22 @@ function chatBot(){
     let user = (document.getElementById("input").value);
 
     //Step 3: write the possible values of the user
-    if (user == "سلام"){
+    if (user == "سلام" ){
 
         //Step 4: show the robot's response
         document.getElementById("respond").innerText = "Bot: " + ("علیک سلام");
     }
 
-    else if (user == "خوبی" ){
-        document.getElementById("respond").innerText = "Bot: " + ("خوبم مرسی");
+    else if (user == "خوبی" || user == "حالت چطوره"){
+        document.getElementById("respond").innerText = "Bot: " + ("خوبم ممنون");
+    }
+
+    else if (user == "خوبم"){
+        document.getElementById("respond").innerText = "Bot: " + ("خداروشکر");
+    }
+
+    else if (user == "خوب نیسم"){
+        document.getElementById("respond").innerText = "Bot: " + ("اخییی");
     }
 
     else if (user == "چخبر"){
@@ -23,15 +31,15 @@ function chatBot(){
         document.getElementById("respond").innerText = "Bot: " + ("اسمم جواده");
     }   
 
-    else if (user == "کجا زندگی میکنی"+name){
-        document.getElementById("respond").innerText = "Bot: " + ("کانادا");
+    else if (user == "کجا زندگی میکنی"){
+        document.getElementById("respond").innerText = "Bot: " + ("در کانادا زندگی میکنم");
     }  
 
-    else if (user == "چند سالته"+name){
-        document.getElementById("respond").innerText = "Bot: " + ("20");
+    else if (user == "چند سالته"){
+        document.getElementById("respond").innerText = "Bot: " + ("20 سالمه");
     }  
 
-    else if (user == "خداحافظ"+name){
+    else if (user == "خداحافظ"){
         document.getElementById("respond").innerText = "Bot: " + ("بعدا میبینمت");
     }   
 
