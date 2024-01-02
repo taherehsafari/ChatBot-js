523c7d96a8a9a7a3bd2da37257136a85debda4c6
tahereh <tahereh@gamil.com>
2024-01-02 04:52:34 -0800

    JavaScript Upgraded
    Three separate functions were used to improve the working process of the bot

--- script.js
+++ script.js
@@ -1,53 +1,59 @@
-//Step 1: define chatBot function
-function chatBot(){
+// Step-1 : specify a function to get input from the user
+function getMessage(){
 
-    //Step 2: get input from the user and get the value using "getElementById"
-    let user = (document.getElementById("input").value);
-
-    //Step 3: write the possible values of the user
-    if (user == "سلام" ){
-
-        //Step 4: show the robot's response
-        document.getElementById("respond").innerText = "Bot: " + ("علیک سلام");
-    }
-
-    else if (user == "خوبی" || user == "حالت چطوره"){
-        document.getElementById("respond").innerText = "Bot: " + ("خوبم ممنون");
-    }
-
-    else if (user == "خوبم"){
-        document.getElementById("respond").innerText = "Bot: " + ("خداروشکر");
-    }
-
-    else if (user == "خوب نیسم"){
-        document.getElementById("respond").innerText = "Bot: " + ("اخییی");
-    }
-
-    else if (user == "چخبر"){
-        document.getElementById("respond").innerText = "Bot: " + ("سلامتی");
-    }   
-
-    else if (user == "اسمت چیه"){
-        document.getElementById("respond").innerText = "Bot: " + ("اسمم جواده");
-    }   
-
-    else if (user == "کجا زندگی میکنی"){
-        document.getElementById("respond").innerText = "Bot: " + ("در کانادا زندگی میکنم");
-    }  
-
-    else if (user == "چند سالته"){
-        document.getElementById("respond").innerText = "Bot: " + ("20 سالمه");
-    }  
-
-    else if (user == "خداحافظ"){
-        document.getElementById("respond").innerText = "Bot: " + ("بعدا میبینمت");
-    }   
-
-    else{
-        document.getElementById("respond").innerText = "Bot: " + ("اطلاعی ندارم");
-    }   
+    // Step-2 : take input from the user and call the processMassage function
+    processMessage(user = (document.getElementById("userInput").value))
 
 }
 
-//Step 5: call the chatBot function
-chatBot()
+// Step-3 : create a function to process the input received from the user
+function processMessage(){
+
+    // Step-4 : use If and Else If to specify that the condition should be executed if the user's input contains a series of words
+    if (user.includes("سلام")){
+
+        // Step-5 : determine the bot's answer and call the showRespond function
+        showRespond( botRespond =  ("علیک سلام"))
+    }
+
+    else if (user.includes("خوبی")){
+
+        showRespond( botRespond =  ("خوبم ممنون"))
+    }
+
+    else if (user.includes("چه خبر")){
+
+        showRespond( botRespond =  ("سلامتی"))
+    }
+
+    else if (user.includes("اسمت چیه")){
+
+        showRespond( botRespond =  ("اسمم جواده"))
+    }
+
+    else if (user.includes("چند سالته")){
+
+        showRespond( botRespond =  ("چند روزی میشه که به وجود اومدم"))
+    }
+
+    else if (user.includes("کجا زندگی میکنی")){
+
+        showRespond( botRespond =  ("توی اینترنت"))
+    }
+
+    else if (user.includes("خداحافظ")){
+
+        showRespond( botRespond =  ("به امید دیدار"))
+    }
+}
+
+// Step-5 : create a function to show the bot's respond
+function showRespond(){
+
+    // Step-6 : show the bot's answer on the screen to the user
+    document.getElementById("respond").innerText = "Bot: " + (botRespond);
+
+}
+
+
+
